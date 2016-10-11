d608a1335d2c5c73215417d1f2d80082940c227f
ShyshkinOleh <shyshkinoleh@yandex.ru>
2016-10-11 18:43:08 +0300

    Update add_headers.py

--- add_headers.py
+++ add_headers.py
@@ -2,7 +2,7 @@ f = open("./data/QUIC_Fact_CTR.csv", "r")
 contents = f.readlines()
 f.close()
 
-contents.insert(0, 'CUSTOMER_ID\tDAY_ID\tTRANSACTION_TM\tSTORE_CITY\tSTORE_PROV\tSTORE_ONSITE_PETRO\tDIVISION_CD\tDIVISION_NM\tAUTO_INVOICE\tREG_PROMO\tNET_POS_D\tTXN_COUNT]')
+contents.insert(0, 'CUSTOMER_ID\tDAY_ID\tTRANSACTION_TM\tSTORE_CITY\tSTORE_PROV\tSTORE_ONSITE_PETRO\tDIVISION_CD\tDIVISION_NM\tAUTO_INVOICE\tREG_PROMO\tNET_POS_D\tTXN_COUNT\n')
 
 f = open("./data/QUIC_Fact_CTR.csv", "w")
 contents = "".join(contents)
